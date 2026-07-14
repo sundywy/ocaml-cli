4cfaa1a4c5c0413e3cf8abd6fda2273ce0e858df
sundy <3445849+sundywy@users.noreply.github.com>
2026-07-14 16:17:23 +0800

    feat: refactor with cursor

--- bin/catr/main.ml
+++ bin/catr/main.ml
@@ -1,66 +1,80 @@
 open Cmdliner
 
-let format_line i line = Format.sprintf "%6d\t%s" i line
+type numbering = Off | All | Nonblank
 
-exception Catr of string
+let format_line i line = Printf.sprintf "%6d\t%s" i line
 
-let term =
-  let open Cmdliner.Term.Syntax in
-  let+ files =
-    let doc = "Input file(s)" in
-    Arg.(value & pos_all string [] & info [] ~docv:"FILE" ~doc)
-  and+ number_lines =
-    let doc = "Number lines" in
-    Arg.(value & flag & info [ "n"; "number" ] ~doc)
-  and+ number_nonblank_lines =
-    let doc = "Number nonblank lines" in
-    Arg.(value & flag & info [ "b"; "number-nonblank-lines" ] ~doc)
+let open_input = function
+  | "-" -> Ok (stdin, false)
+  | path -> (
+      try Ok (In_channel.open_text path, true)
+      with Sys_error msg -> Error msg)
+
+let print_lines numbering ic =
+  let rec loop i =
+    match In_channel.input_line ic with
+    | None -> ()
+    | Some line ->
+        let next =
+          match numbering with
+          | Off ->
+              print_endline line;
+              i
+          | All ->
+              print_endline (format_line i line);
+              i + 1
+          | Nonblank when String.length line = 0 ->
+              print_newline ();
+              i
+          | Nonblank ->
+              print_endline (format_line i line);
+              i + 1
+        in
+        loop next
   in
-  if number_lines && number_nonblank_lines then
-    raise (Catr "Only use number_lines or number_nonblank_lines")
-  else
-    let print_stdin filename =
-      try
-        let f =
-          match filename with
-          | Some filename' -> In_channel.open_text filename'
-          | None -> stdin
-        in
-        let rec print_with_index i =
-          match In_channel.(input_line f) with
-          | Some line ->
-              let j =
-                if number_nonblank_lines && String.length line = 0 then (
-                  print_newline ();
-                  i)
-                else if number_lines || number_nonblank_lines then (
-                  print_endline (format_line i line);
-                  i + 1)
-                else (
-                  print_endline line;
-                  i + 1)
-              in
-              print_with_index j
-          | None -> ()
-        in
-        print_with_index 1;
-        In_channel.close_noerr f
-      with Sys_error str ->
-        prerr_endline str;
-        ()
-    in
-    let rec print_files files =
-      match files with
-      | file :: files' ->
-          print_stdin (Some file);
-          print_files files'
-      | [] -> ()
-    in
-    match files with [] | [ "-" ] -> print_stdin None | _ -> print_files files
+  loop 1
+
+let cat_file numbering path =
+  match open_input path with
+  | Error msg -> prerr_endline msg
+  | Ok (ic, close) ->
+      Fun.protect
+        ~finally:(fun () -> if close then In_channel.close_noerr ic)
+        (fun () -> print_lines numbering ic)
+
+let run files numbering =
+  match files with
+  | [] -> cat_file numbering "-"
+  | _ -> List.iter (cat_file numbering) files
+
+let files =
+  let doc = "Input file(s). Use - for standard input." in
+  Arg.(value & pos_all string [] & info [] ~docv:"FILE" ~doc)
+
+let number =
+  let doc = "Number all output lines" in
+  Arg.(value & flag & info [ "n"; "number" ] ~doc)
+
+let number_nonblank =
+  let doc = "Number nonempty output lines" in
+  Arg.(value & flag & info [ "b"; "number-nonblank" ] ~doc)
+
+let numbering =
+  let combine number number_nonblank =
+    match (number, number_nonblank) with
+    | true, true ->
+        `Error
+          ( true,
+            "can only use one of -n/--number or -b/--number-nonblank" )
+    | true, false -> `Ok All
+    | false, true -> `Ok Nonblank
+    | false, false -> `Ok Off
+  in
+  Term.(ret (const combine $ number $ number_nonblank))
 
 let cmd =
-  let doc = "Rust cat in ocaml" in
+  let doc = "Concatenate and print files" in
   let info = Cmd.info "catr" ~version:"0.1.0" ~doc in
-  Cmd.v info term
+  Cmd.v info Term.(const run $ files $ numbering)
 
 let () = exit (Cmd.eval cmd)
